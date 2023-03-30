56d4f1b91a5ae84c4adba797f6ef471ce365da9a
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-03-30 06:28:39 +0200

    Rendered a base.html template in a index view

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    name = request.GET.get("name") or "world"
-    return HttpResponse(f"Hello, {name}!")
+    return render(request, "base.html")
