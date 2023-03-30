f6c78feeeb82a272e8704a7a175504e7701ec8e1
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-03-30 06:48:56 +0200

    Used name variable in the base.html template

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "base.html")
+    name = "world"
+    return render(request, "base.html", {"name": name})
