b9b5a355a614427c0d8c5047d67156b709d6553e
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-03-31 20:54:20 +0200

    Book search scaffold

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     name = "world"
     return render(request, "base.html", {"name": name})
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html",
+                  {"search_text": search_text})
