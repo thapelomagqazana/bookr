a211060df2a3878ada6330f5a00140eb79d7d123
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-04-02 01:07:33 +0200

    Created Publisher and Contributor models

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 class Publisher(models.Model):
     """A company that publishes books"""
     name = models.CharField(max_length=50, help_text="The name of the Publisher.")
     website = models.URLField(help_text="The Publisher's website")
     email = models.EmailField(help_text="The Publisher's email.")
+
+
+class Book(models.Model):
+    """A published book"""
+    title = models.CharField(max_length=70, help_text="The title of the book.", )
+    publication_date = models.DateField(verbose_name="Date the book was published.")
+    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
+
+
+class Contributor(models.Model):
+    """A contributor to a Book, e.g author, editor, co-author"""
+    first_names = models.CharField(max_length=50, help_text="The Contributor's first name or names.")
+    last_names = models.CharField(max_length=50, help_text="The Contributor's last name or names.")
+    email = models.EmailField(help_text="The contact email of the contributor.")
