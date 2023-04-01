20d75640ae80632e7d32651353dab8ac3bdc0a20
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-04-02 01:15:35 +0200

    Created Many to One Relationship

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
     title = models.CharField(max_length=70, help_text="The title of the book.", )
     publication_date = models.DateField(verbose_name="Date the book was published.")
     isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
-
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 
 class Contributor(models.Model):
     """A contributor to a Book, e.g author, editor, co-author"""
