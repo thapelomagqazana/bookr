a90c4583ae41ccdf8dbb0f17f6af0a20c2bd7fbb
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-04-02 15:45:45 +0200

    Added Review model

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 
 # Create your models here.
@@ -15,9 +16,32 @@ class Book(models.Model):
     publication_date = models.DateField(verbose_name="Date the book was published.")
     isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    contributor = models.ManyToManyField('Contributor', through="BookContributor")
 
 class Contributor(models.Model):
     """A contributor to a Book, e.g author, editor, co-author"""
     first_names = models.CharField(max_length=50, help_text="The Contributor's first name or names.")
     last_names = models.CharField(max_length=50, help_text="The Contributor's last name or names.")
     email = models.EmailField(help_text="The contact email of the contributor.")
+
+class BookContributor(models.Model):
+    class ContributionRole(models.TextChoices):
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(verbose_name="The role this contributor had in the book.",
+                            choices=ContributionRole.choices, max_length=20)
+
+    class Review(models.Model):
+        content = models.TextField(help_text="The Review text.")
+        rating = models.IntegerField(help_text="The rating the reviewer has given.")
+        date_created = models.DateTimeField(auto_now_add=True,
+                                            help_text="The date and time the review was created.")
+        date_edited = models.DateField(null=True,
+                                       help_text="The date and time the review was edited.")
+        creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+        book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                                 help_text="The Book that this review is for.")
