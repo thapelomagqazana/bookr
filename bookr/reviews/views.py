216a1537706b17a730964641046b55685539ee1c
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-04-07 18:01:45 +0200

    Modified base.html to display 'Welcome to Bookr!' on the Home Page

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 
 # Create your views here.
-def index(request):
-    name = "world"
-    return render(request, "base.html", {"name": name})
+# def index(request):
+#     name = "world"
+#     return render(request, "base.html", {"name": name})
+#
+#
+# def book_search(request):
+#     search_text = request.GET.get("search", "")
+#     return render(request, "search-results.html",
+#                   {"search_text": search_text})
 
-
-def book_search(request):
-    search_text = request.GET.get("search", "")
-    return render(request, "search-results.html",
-                  {"search_text": search_text})
+def welcome_view(request):
+    return render(request, 'base.html')
