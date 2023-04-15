340659bbd4cba04bfbfabbe4bdadd52df1a4b913
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-04-15 09:43:28 +0200

    Modified the Contributors list display to return intialled names.

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 
 from reviews.models import (Book, Publisher, Contributor, BookContributor,Review)
 
+def initialled_name(obj):
+    """ obj.first_names='Jerome David' , obj.last_names='Salinger'
+    => 'Salinger, JD' """
+    initials = ''.join([name[0] for name in \
+                        obj.first_names.split(' ')])
+    return f"{obj.last_names}, {initials}"
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (initialled_name,)
+
 class BookrAdminSite(AdminSite):
     title_header = 'Bookr Admin'
     site_header = 'Book Administration'
@@ -16,6 +26,6 @@ admin_site = BookrAdminSite(name='bookr')
 # Register your models here.
 admin_site.register(Book, BookAdmin)
 admin_site.register(Publisher)
-admin_site.register(Contributor)
+admin_site.register(Contributor, ContributorAdmin)
 admin_site.register(BookContributor)
 admin_site.register(Review)
