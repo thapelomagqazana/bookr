e458743b0fd84e320ed7662c52b0b163a23cdde3
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-04-15 09:29:05 +0200

    Modified the Books list display with a two-column list display

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -8,13 +8,13 @@ class BookrAdminSite(AdminSite):
     site_header = 'Book Administration'
     index_title = 'Bookr site admin'
 
-# class BookAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'isbn')
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn')
 
 admin_site = BookrAdminSite(name='bookr')
 
 # Register your models here.
-admin_site.register(Book)
+admin_site.register(Book, BookAdmin)
 admin_site.register(Publisher)
 admin_site.register(Contributor)
 admin_site.register(BookContributor)
