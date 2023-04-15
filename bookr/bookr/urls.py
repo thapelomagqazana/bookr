1698ac55cc5146bed8d2db5943bba2eddc85c124
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-04-15 09:27:07 +0200

    Changed the Books list display by including the ISBN field using the Model __str__(string) representation

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from reviews.admin import admin_site
 from django.contrib import admin
 from django.urls import path, include
 import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),\
+    path('bookradmin/', admin_site.urls),
     path('', include("reviews.urls")),
 ]
--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,3 +1,21 @@
+from django.contrib.admin import AdminSite
 from django.contrib import admin
 
+from reviews.models import (Book, Publisher, Contributor, BookContributor,Review)
+
+class BookrAdminSite(AdminSite):
+    title_header = 'Bookr Admin'
+    site_header = 'Book Administration'
+    index_title = 'Bookr site admin'
+
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'isbn')
+
+admin_site = BookrAdminSite(name='bookr')
+
 # Register your models here.
+admin_site.register(Book)
+admin_site.register(Publisher)
+admin_site.register(Contributor)
+admin_site.register(BookContributor)
+admin_site.register(Review)
